41d0e586458e38742bf92479ecdca5d136bf98c5
Yul3102 <YOUR-EMAIL-ADDRESS>
2022-05-24 23:06:59 -0400

    changed all class characters to rectangle image from circle color

--- gameOne.js
+++ gameOne.js
@@ -4,7 +4,7 @@ const container = document.getElementById('container');
     const square = document.createElement('div');
     container.append(square);
     //square.style.attribute
-    square.style.border = 'solid 2px black'
+    //square.style.border = 'solid 2px black'
     square.style.borderRadius = '10px'
     square.style.backgroundColor = 'white'
     square.style.width = "250px"
@@ -58,9 +58,13 @@ const container = document.getElementById('container');
 let game = document.querySelector("#game");
 let zverev;
 let r = 20;//!!this variable is deciding player circle's radius & movement speed!!
-
-// this creates a 2 dimensional canvas =>
-let ctx = game.getContext("2d");
+let rs = 40;
+let os =30;
+let rv = Math.floor(Math.random()*7-4);
+let rrv = Math.floor(Math.random()*8);
+let arrayR = [20,-20,-5,5,-10,35,-60,40]
+// this creates a 2 dimensional canvas => 
+let ctx = game.getContext("2d"); 
 game.setAttribute("height", getComputedStyle(game)["height"]);
 game.setAttribute("width", getComputedStyle(game)["width"]);
 
@@ -69,31 +73,9 @@ document.addEventListener("keydown", movementHandler)
 //    restart.addEventListener("click",location.reload())
 // restart.addEventListener("keypress", space)
 
-
+ 
   
 /*=================== class Collections======================*/
-
-//Build a player class
-class Player{
-    constructor(x,y,radius,color,dx,dy){
-        this.x = x;
-        this.y = y;
-        this.radius = radius;
-        this.color = color;
-        this.alive = true;
-        this.dx = dx;
-        this.dy = dy;
-    }
-    //adding player's function: render & moving(location change) 
-
-    render(){
-        ctx.beginPath();
-        ctx.arc(this.x,this.y,this.radius,0,2*Math.PI,false);
-        ctx.fillStyle = this.color;
-        ctx.fill();
-    }
-  
-}
 
 class ImagePlayer{
     constructor(x,y,width,height,radius){
@@ -101,97 +83,92 @@ class ImagePlayer{
         this.y = y;
         this.width = width;
         this.height = height;
-        this.radius = radius;
+         this.radius = radius;
+        this.alive = true;
     }
     draw(){
-       
      ctx.drawImage(imgP,this.x,this.y,this.width,this.height)
-    //  round.borderRadius ='10px'
-    //  ctx.drawImage(imgP, 0, 0, 40, 40, 30, 40, 40, 40);
-    //  ctx.globalCompositeOperation = 'destination-in';
-    //  ctx.fillStyle = this.color;
-    //  ctx.beginPath();
-    //  ctx.arc(this.x,this.y,this.radius,0,2*Math.PI,false);
-    //  ctx.fill();
-    //  ctx.globalCompositeOperation = 'source-over';
     }
-    
+    // draw2(){
+    //     ctx.drawImage(imgP2,this.x,this.y,this.width,this.height)
+    //    }
+    // draw3(){
+    //     ctx.drawImage(imgP3,this.x,this.y,this.width,this.height)
+    //    }
 }
 
 //Build a obstacle class
 class Obstacle{
-    constructor(x,y,radius,color,velocity){
+    constructor(x,y,width,height,velocity){
         this.x = x;
         this.y = y;
-        this.radius = radius;
-        this.color = color;
+        this.width = width;
+        this.height = height;
         this.velocity = velocity;
         this.alive = true;
     }
     //adding obstacle's function: render 
 
     render(){
-        ctx.beginPath();
-        ctx.arc(this.x,this.y,this.radius,0,2*Math.PI,false);
-        ctx.fillStyle = this.color;
-        ctx.fill();
-    }
-    //moving automatically
+        ctx.drawImage(ballY,this.x,this.y,this.width,this.height)
+       }
+    renderOrange(){
+        ctx.drawImage(ballO,this.x,this.y,this.width,this.height)
+       }
     //Ball bounce if it hits a wall
     //Ball goes back to the top and restart dropping if it hits bottom
-    launch(){
+    launchYellow(){
         this.render();
         this.x += this.velocity.x;
         this.y += this.velocity.y;
-        if(this.x + this.radius > game.width || this.x-this.radius<0){
+        if(this.x + this.width > game.width || this.x <= 0){
           this.velocity.x = -1*this.velocity.x  
-        }else if(this.y - this.radius > game.height){
+        }else if(this.y - this.width > game.height){
             this.y = 0;
-            this.velocity.x = Math.floor(Math.random()*9-4)
+            // this.velocity.x = Math.floor(Math.random()*9-4)
+            this.velocity.x =rv
             this.y += this.velocity.y;
         }
-
     }
 
-    launchTwo(){
-        this.render();
+    launchOrange(){
+        this.renderOrange();
         this.x += this.velocity.x;
         this.y += this.velocity.y;
-        if(this.x + this.radius > game.width || this.x-this.radius<0){
+        if(this.x + this.width > game.width || this.x <= 0){
           this.velocity.x = -1*this.velocity.x  
-        }else if(this.y - this.radius > game.height){
+        }else if(this.y - this.width > game.height){
             this.y = -40;
             this.velocity.x = Math.floor(Math.random()*10+10)
             this.y += this.velocity.y;
         }
-
     }
-
 }
+
 //Build another type of obstacle class
 class topSpin{
-    constructor(x,y,radius,color){
+    constructor(x,y,width,height){
         this.x = x;
         this.y = y;
-        this.radius = radius;
-        this.color = color;
+        this.width = width;
+        this.height = height;
         this.alive = true;
     }
     //adding obstacle's function: render 
 
-    render(){
-        ctx.beginPath();
-        ctx.arc(this.x,this.y,this.radius,0,2*Math.PI,false);
-        ctx.fillStyle = this.color;
-        ctx.fill();
-    }
+    renderRed(){
+        ctx.drawImage(ballR,this.x,this.y,this.width,this.height)
+       }
+    renderPurple(){
+        ctx.drawImage(ballP,this.x,this.y,this.width,this.height)
+       }
     //moving automatically
     //Ball goes straight down
     //Ball goes back to the top and restart dropping if it hits bottom
     launch(){
-        this.render();
+        this.renderRed();
         this.y += 25 
-        if(this.y - this.radius > game.height){
+        if(this.y - this.width > game.height){
            setTimeout(()=>{ 
             //adding a little delay 
             this.y = -60;
@@ -200,24 +177,33 @@ class topSpin{
         },100)
         } 
     }
+    launchP(){
+        this.renderPurple();
+        this.y += 20 
+        if(this.y - this.width > game.height){
+            this.y = -60;
+            this.x = Math.floor(Math.random()*(game.width-rs)/1.5)
+            this.y += 30; 
+        } 
+    }
+    
 
 }
 /*======== Build Array =======*/
 let allBalls = [];
 /*=================== Game Player Constructions ======================*/
 //construct game players here.
-const you = new Player(game.width/2,game.height-r,r,'grey')
+// const you = new Player(game.width/2,game.height-r,r,'grey')
 // ball always comes down from the top and x rocation alters randomly everytime.
 //velocity is an object which has x and y value so I can control the speed and angle of the ball movement.
-let rv = Math.floor(Math.random()*15-5);
-let rrv = Math.floor(Math.random()*9-5);
-// let rrv = Math.floor(Math.random()*13+15);
-const ball = new Obstacle(Math.floor(Math.random()*(game.width-30)),-30,15,'#FFFF00',{x:rv,y:25})
-const ballTwo = new Obstacle(Math.floor(Math.random()*(game.width-30)),-30,15,'#FFFF00',{x:rrv,y:25})
-const curve = new topSpin(Math.floor(Math.random()*(game.width-30)),-30,15,'#dfff4f')
-const curveTwo = new topSpin(Math.floor(Math.random()*(game.width-30)),-30,15,'#dfff4f')
 
-const smile = new ImagePlayer(game.width/2,game.height-40,40,40,40);
+// let rrv = Math.floor(Math.random()*13+15);
+const yellow = new Obstacle(Math.floor(Math.random()*(game.width-30)),-30,os,os,{x:rv,y:20})
+const orange = new Obstacle(Math.floor(Math.random()*(game.width-30)),-30,os,os,{x:arrayR[rrv],y:20})
+const red = new topSpin(Math.floor(Math.random()*(game.width-30)),-30,os,os)
+const purple = new topSpin(Math.floor(Math.random()*(game.width-30)),-30,os,os)
+
+const smile = new ImagePlayer((game.width-rs)/2,game.height-rs,rs,rs,rs);
 
 
 
@@ -225,22 +211,22 @@ const smile = new ImagePlayer(game.width/2,game.height-40,40,40,40);
 //KEYBOARD INTERACTION LOGIC
 function movementHandler(e) {
     console.log("the key that was pressed was:" + e.key)
-    console.log(you.y)
+    console.log(smile.y)
 
     switch (e.key) {
         case "ArrowUp":
-            you.y-2*you.radius > 0 ? you.y -= 2*r: null;
+            smile.y > 0 ? smile.y -= rs: null;
             break
         case "ArrowDown":
-            (you.y + 2*you.radius) < game.height ? you.y += 2*r : null;
+            (smile.y + smile.height) < game.height ? smile.y += rs : null;
             break
         case "ArrowLeft":
-           ( you.x-2*you.radius ) >= 0 ? you.x -= 2*r : null;
-           console.log(you.x)
+           ( smile.x ) >= 0 ? smile.x -= rs: null;
+           console.log(smile.x)
             break
         case "ArrowRight":
-            (you.x +2*you.radius ) <= game.width ? you.x += 2*r: null;
-            console.log(you.x)
+            (smile.x + smile.width ) <= game.width ? smile.x += rs: null;
+            console.log(smile.x)
             break
         case ' ' :
                 restarting();
@@ -274,33 +260,35 @@ function movementHandler(e) {
 function gameLoop() {
     ctx.clearRect(0, 0, game.width, game.height);
 
-    if(you.alive){
-    curve.launch();
-    ball.launch();
-    ballTwo.launchTwo(); 
-   
-    detectHit(you,curve);  
-    detectHit(you,ball);
-    detectHit(you,ballTwo);
-    victory(); 
-    if(seconds > 10 ){
-        curveTwo.launch();
-        detectHit(you,curveTwo);
-    }}
-
-    if(seconds > 15 ){
-        curveTwo.color = 'red';
-        tShot(you,curveTwo)
-    }
-    you.render();
-    smile.draw();
-
-    // for(i=0;i<allBalls.length;i++){
-    //     console.log(allBalls)
-    //     allBalls[i].launch()
-    // } 
+    if(smile.alive){
+    yellow.launchYellow();
+    red.launch();
     
+    detectHit(smile,yellow);  
+    detectHit(smile,red);  
+    if(seconds >= 5){
+        orange.launchOrange();
+        detectHit(smile,orange);
+    }
+    if(seconds >= 15){
+        purple.launchP();
+        tShot(smile,purple) 
+        detectHit(smile,purple);
+    }
+    }
+    
+    // else if(seconds >= 15){
+    //     purple.launchP();   
+    //     detectHit(smile,purple);
+    //     tShot(smile,purple) 
+    // }
+    
+
+    victory(); 
+    smile.draw();
 }
+
+
 //Detect hit between player and obstacles
 function circleD (p1,p2){
     let xD = p1.x -p2.x ;
@@ -309,8 +297,14 @@ function circleD (p1,p2){
 }
 
 function detectHit(p1,p2) {
+
+    let hitTest =  p1.y + p1.height > p2.y &&
+    p1.y < p2.y + p2.height &&
+    p1.x + p1.width > p2.x &&
+    p1.x < p2.x + p2.width; 
   
-    if (circleD(p1,p2) < p1.radius+p2.radius) {
+    // if (circleD(p1,p2) < p1.width/2+p2.radius) {
+    if (hitTest) {
         p1.alive = false;
         p2.alive = false;
       
@@ -333,7 +327,7 @@ function tShot(p1,p2){
 //Game over Text
 function gameOver(){
    
-    if(you.alive){
+    if(smile.alive){
         square.style.display = "none"
     }else{
         textTag.innerText = "Oh...No...\n GameOver!"
@@ -373,7 +367,7 @@ function runGame() {
 
 
 function timeCount(){
-    if(you.alive)
+    if(smile.alive)
   {  seconds +=1;
     secondText.innerText = "Time\n"+ seconds}
 }
@@ -430,3 +424,117 @@ setInterval(timeCount,1000)
 
 
 
+
+
+
+/*============ bin ==================*/
+
+
+// //Build a obstacle class
+// class Obstacle{
+//     constructor(x,y,radius,color,velocity){
+//         this.x = x;
+//         this.y = y;
+//         this.radius = radius;
+//         this.color = color;
+//         this.velocity = velocity;
+//         this.alive = true;
+//     }
+//     //adding obstacle's function: render 
+
+//     render(){
+//         ctx.beginPath();
+//         ctx.arc(this.x,this.y,this.radius,0,2*Math.PI,false);
+//         ctx.fillStyle = this.color;
+//         ctx.fill();
+//     }
+//     //moving automatically
+//     //Ball bounce if it hits a wall
+//     //Ball goes back to the top and restart dropping if it hits bottom
+//     launch(){
+//         this.render();
+//         this.x += this.velocity.x;
+//         this.y += this.velocity.y;
+//         if(this.x + this.radius > game.width || this.x-this.radius<0){
+//           this.velocity.x = -1*this.velocity.x  
+//         }else if(this.y - this.radius > game.height){
+//             this.y = 0;
+//             this.velocity.x = Math.floor(Math.random()*9-4)
+//             this.y += this.velocity.y;
+//         }
+
+//     }
+
+//     launchTwo(){
+//         this.render();
+//         this.x += this.velocity.x;
+//         this.y += this.velocity.y;
+//         if(this.x + this.radius > game.width || this.x-this.radius<0){
+//           this.velocity.x = -1*this.velocity.x  
+//         }else if(this.y - this.radius > game.height){
+//             this.y = -40;
+//             this.velocity.x = Math.floor(Math.random()*10+10)
+//             this.y += this.velocity.y;
+//         }
+
+//     }
+
+// }
+// //Build another type of obstacle class
+// class topSpin{
+//     constructor(x,y,radius,color){
+//         this.x = x;
+//         this.y = y;
+//         this.radius = radius;
+//         this.color = color;
+//         this.alive = true;
+//     }
+//     //adding obstacle's function: render 
+
+//     render(){
+//         ctx.beginPath();
+//         ctx.arc(this.x,this.y,this.radius,0,2*Math.PI,false);
+//         ctx.fillStyle = this.color;
+//         ctx.fill();
+//     }
+//     //moving automatically
+//     //Ball goes straight down
+//     //Ball goes back to the top and restart dropping if it hits bottom
+//     launch(){
+//         this.render();
+//         this.y += 25 
+//         if(this.y - this.radius > game.height){
+//            setTimeout(()=>{ 
+//             //adding a little delay 
+//             this.y = -60;
+//             this.x = Math.floor(Math.random()*(game.width-30))
+//             this.y += 30;
+//         },100)
+//         } 
+//     }
+
+// }
+
+
+// //Build a player class
+// class Player{
+//     constructor(x,y,radius,color,dx,dy){
+//         this.x = x;
+//         this.y = y;
+//         this.radius = radius;
+//         this.color = color;
+//         this.alive = true;
+//         this.dx = dx;
+//         this.dy = dy;
+//         this.alive = true;
+//     }
+//     //adding player's function: render & moving(location change) 
+
+//     render(){
+//         ctx.beginPath();
+//         ctx.arc(this.x,this.y,this.radius,0,2*Math.PI,false);
+//         ctx.fillStyle = this.color;
+//         ctx.fill();
+//     }
+  
+// }
