f11f743730c890dbf6729bae6851e5dc28a658e5
Yul3102 <YOUR-EMAIL-ADDRESS>
2022-05-25 12:50:52 -0400

    starting button has been added.// discovered restarting bug related to setinterval. Everything moves faster after restarting. Solved the bug by using reloading commands  within restarting function.

--- gameOne.js
+++ gameOne.js
@@ -1,10 +1,10 @@
 /*========= DOM ========*/
 const container = document.getElementById('container');
-    //create a 'div'tag
+//create a 'div'tag
 const square = document.createElement('div');
     container.append(square);
-    //square.style.attribute
-    //square.style.border = 'solid 2px black'
+ //square.style.attribute
+//square.style.border = 'solid 2px black'
     square.style.borderRadius = '10px'
     square.style.backgroundColor = 'white'
     square.style.width = "250px"
@@ -15,20 +15,30 @@ const square = document.createElement('div');
     square.style.transform = 'translate(-50%,-50%)'
     square.style.textAlign = 'center'
    
-    //create a 'text tag'
+//create a 'text tag'
 const textTag = document.createElement('h3')
     
     square.append(textTag)
     //textTag.style.attribute
     textTag.style.padding= '5px 5px 5px 5px';
-   //create a 'restart button'
+//Create a 'Start Button'
+const start = document.createElement('div')
+    start.setAttribute('id','start')
+    start.innerText = "Start"
+    start.style.border = "solid 1px black"
+    start.style.fontSize = '30px'
+    start.style.padding = '5px 40px 5px 40px'
+    start.style.borderRadius = '10px'
+    container.append(start)
+
+//create a 'restart button'
 const restart = document.createElement('button')
    restart.innerText = "Restart\n(space key)"
    square.append(restart)
    square.style.display = "none" 
 
-   // create a time count board tags and texts
-   const board = document.getElementById('board');
+// create a time count board tags and texts
+const board = document.getElementById('board');
    let secondText = document.createElement('h3');
    board.append(secondText);
    let seconds = 0;
@@ -71,8 +81,11 @@ game.setAttribute("width", getComputedStyle(game)["width"]);
 
 //controler
 document.addEventListener("keydown", movementHandler)
-//    restart.addEventListener("click",location.reload())
-// restart.addEventListener("keypress", space)
+
+start.addEventListener("click",() => {
+    // start.style.display = 'none'
+    gameInit();})
+
 
  
   
@@ -261,25 +274,49 @@ function movementHandler(e) {
 
 
 /* ================= functions ========================*/ 
-// function constructingBalls(){
-//     let rv = Math.floor(Math.random()*9-18);
-//     let rrv = Math.floor(Math.random()*13+5);
-//     while(allBalls.length<20){
-//     const newBall = new Obstacle(Math.floor(Math.random()*(game.width-30)),30,30,'#FFFF00',{x:rv,y:25});
-//     allBalls.push(newBall);
-//     allBalls.length++;
-    
-// }}
 
+//Restarting
+function restarting(){
+   window.onload()
+   reloadP()
+    }
+
+//Game Initiation//
+function gameInit(){
+        start.removeEventListener("click",() => {
+            gameInit();})
+        start.style.display = 'none'
+        game.style.display = "block";
+        board.style.display = "block" 
+        
+        setInterval(timeCount,1000)
+        runGame(); 
+    }
+//reloading
+    window.onload = function() {
+        var reloading = sessionStorage.getItem("reloading");
+        if (reloading) {
+            sessionStorage.removeItem("reloading");
+            gameInit();
+        }
+    }
+    
+    function reloadP() {
+        sessionStorage.setItem("reloading", "true");
+        document.location.reload();
+    }
+
+    
+//Game Loop
 function gameLoop() {
     ctx.clearRect(0, 0, game.width, game.height);
 
-    if(smile.alive){
+    if(smile.alive){    
     yellow.launchYellow();
-    red.launch();
-    
+    red.launch()
     detectHit(smile,yellow);  
     detectHit(smile,red);  
+
     if(seconds >= 5){
         orange.launchOrange();
         detectHit(smile,orange);
@@ -289,13 +326,7 @@ function gameLoop() {
         tShot(smile,purple) 
         detectHit(smile,purple);
     }
-    }
-    
-    // else if(seconds >= 15){
-    //     purple.launchP();   
-    //     detectHit(smile,purple);
-    //     tShot(smile,purple) 
-    // }
+    }else{ctx.clearRect(0, 0, game.width, game.height)}
     
     
     victory(); 
@@ -322,7 +353,6 @@ function detectHit(p1,p2) {
         p1.alive = false;
         p2.alive = false;
       
-    //    document.removeEventListener("keydown", movementHandler);
        gameOver() ;   
     } 
 }  
@@ -361,200 +391,23 @@ function victory(){
     }
    
 }
-function restarting(){
- location.reload()
 
-}
 //Run Game
 function runGame() {  
     setInterval(gameLoop,45);
+    
 }
-//Count time
-// function drawTime(){
-//    const board = document.getElementById('board');
-//    let secondText = document.createElement('h3');
-//    let seconds = 0;
-//    board.innerHTML = "Time:"+time()
- 
-// }
-
 
 
 function timeCount(){
     if(smile.alive)
   {  seconds +=1;
     secondText.innerText = "Time\n"+ seconds}
-}
-//Game Initiation//
-function gameInit(){
-    game.style.display = "block";
-    board.style.dispay = "block";
-    setInterval(timeCount,1000)
-    runGame();
-}
-
-
-// function constructingBalls(){
-//     setInterval(() => {
-//     let rv = Math.floor(Math.random()*9-5);
-//     let rrv = Math.floor(Math.random()*13+5);
-//     const newBall = new Obstacle(Math.floor(Math.random()*(game.width-30)),-30,30,'#FFFF00',{x:rv,y:rrv});
-//     allBalls.push(newBall);
-//     console.log(allBalls)
-
-// },500)
-// ;
-// }
-
-/* ================= Running Game ========================*/ 
-
-
-
-// constructingBalls()
-
-
-
-
-
-// for(i=0;i<allBalls.length;i++){
-//     console.log(allBalls)
-//     allBalls[i].launch()
-// } 
-
-
-
-
-
-
-//Bounce after hitting sidewalls
-
-
-
-
-//==============EventListner==================//
-
-//DOM Content Loaded EventListener
-// window.addEventListener("DOMContentLoaded", function (e) {
-//     player = new Crawler((game.width - 20) / 2, game.height - 20, "white", 20, 20);
-//     circle = new Ball(xRandom,0, "yellow", 10, 10)
-//     const cTwo = new BallTwo(300,100,30,'blue')
    
-//     const runGame = setInterval(gameLoop, 100);
-// })
-
-
+}
 
 
 
 
 /*============ bin ==================*/
 
-
-// //Build a obstacle class
-// class Obstacle{
-//     constructor(x,y,radius,color,velocity){
-//         this.x = x;
-//         this.y = y;
-//         this.radius = radius;
-//         this.color = color;
-//         this.velocity = velocity;
-//         this.alive = true;
-//     }
-//     //adding obstacle's function: render 
-
-//     render(){
-//         ctx.beginPath();
-//         ctx.arc(this.x,this.y,this.radius,0,2*Math.PI,false);
-//         ctx.fillStyle = this.color;
-//         ctx.fill();
-//     }
-//     //moving automatically
-//     //Ball bounce if it hits a wall
-//     //Ball goes back to the top and restart dropping if it hits bottom
-//     launch(){
-//         this.render();
-//         this.x += this.velocity.x;
-//         this.y += this.velocity.y;
-//         if(this.x + this.radius > game.width || this.x-this.radius<0){
-//           this.velocity.x = -1*this.velocity.x  
-//         }else if(this.y - this.radius > game.height){
-//             this.y = 0;
-//             this.velocity.x = Math.floor(Math.random()*9-4)
-//             this.y += this.velocity.y;
-//         }
-
-//     }
-
-//     launchTwo(){
-//         this.render();
-//         this.x += this.velocity.x;
-//         this.y += this.velocity.y;
-//         if(this.x + this.radius > game.width || this.x-this.radius<0){
-//           this.velocity.x = -1*this.velocity.x  
-//         }else if(this.y - this.radius > game.height){
-//             this.y = -40;
-//             this.velocity.x = Math.floor(Math.random()*10+10)
-//             this.y += this.velocity.y;
-//         }
-
-//     }
-
-// }
-// //Build another type of obstacle class
-// class topSpin{
-//     constructor(x,y,radius,color){
-//         this.x = x;
-//         this.y = y;
-//         this.radius = radius;
-//         this.color = color;
-//         this.alive = true;
-//     }
-//     //adding obstacle's function: render 
-
-//     render(){
-//         ctx.beginPath();
-//         ctx.arc(this.x,this.y,this.radius,0,2*Math.PI,false);
-//         ctx.fillStyle = this.color;
-//         ctx.fill();
-//     }
-//     //moving automatically
-//     //Ball goes straight down
-//     //Ball goes back to the top and restart dropping if it hits bottom
-//     launch(){
-//         this.render();
-//         this.y += 25 
-//         if(this.y - this.radius > game.height){
-//            setTimeout(()=>{ 
-//             //adding a little delay 
-//             this.y = -60;
-//             this.x = Math.floor(Math.random()*(game.width-30))
-//             this.y += 30;
-//         },100)
-//         } 
-//     }
-
-// }
-
-
-// //Build a player class
-// class Player{
-//     constructor(x,y,radius,color,dx,dy){
-//         this.x = x;
-//         this.y = y;
-//         this.radius = radius;
-//         this.color = color;
-//         this.alive = true;
-//         this.dx = dx;
-//         this.dy = dy;
-//         this.alive = true;
-//     }
-//     //adding player's function: render & moving(location change) 
-
-//     render(){
-//         ctx.beginPath();
-//         ctx.arc(this.x,this.y,this.radius,0,2*Math.PI,false);
-//         ctx.fillStyle = this.color;
-//         ctx.fill();
-//     }
-  
-// }
