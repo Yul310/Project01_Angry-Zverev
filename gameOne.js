fbfa5d9de12cf6547564de7ec41539f491a7adb9
Yul3102 <YOUR-EMAIL-ADDRESS>
2022-05-26 23:02:58 -0400

    file updating

--- gameOne.js
+++ gameOne.js
@@ -362,13 +362,13 @@ function movementHandler(e) {
     switch (e.key) {
         case "ArrowUp":
 <<<<<<< HEAD
-            smile.y > 0 ? smile.y -= (10+rs): null;
+            smile.y > 0 ? smile.y -= (0+rs): null;
             break
         case "ArrowDown":
-            (smile.y + smile.height) < game.height ? smile.y += (10+rs): null;
+            (smile.y + smile.height) < game.height ? smile.y += (0+rs): null;
             break
         case "ArrowLeft":
-           ( smile.x ) >= -100 ? smile.x -= (10+rs): null;
+           ( smile.x ) >= -100 ? smile.x -= (0+rs): null;
 =======
             smile.y > 0 ? smile.y -= (0+rs): null;
             break
@@ -383,7 +383,7 @@ function movementHandler(e) {
        
         case "ArrowRight":
 <<<<<<< HEAD
-            (smile.x + smile.width ) <= game.width ? smile.x += (10+rs): null;
+            (smile.x + smile.width ) <= game.width ? smile.x += (0+rs): null;
 =======
             (smile.x + smile.width ) <= game.width ? smile.x += (0+rs): null;
 >>>>>>> main
